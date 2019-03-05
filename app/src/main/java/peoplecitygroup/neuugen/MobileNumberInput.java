b3261c87e937d2363c28e44a90ac43f764769633
pranjal012 <pranjalv206@gmail.com>
2019-03-06 00:37:29 +0530

    ghi

--- app/src/main/java/peoplecitygroup/neuugen/MobileNumberInput.java
+++ app/src/main/java/peoplecitygroup/neuugen/MobileNumberInput.java
@@ -2,13 +2,65 @@ package peoplecitygroup.neuugen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 
-public class MobileNumberInput extends AppCompatActivity {
+import static android.os.Build.VERSION_CODES.JELLY_BEAN;
+
+public class MobileNumberInput extends AppCompatActivity implements View.OnClickListener {
+
+    com.google.android.material.textfield.TextInputEditText phonenum;
+    com.google.android.material.button.MaterialButton nextbtn;
+    String phonetext;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mobile_number_input);
+
+        idLink();
+        listenerLink();
+
+    }
+
+    public void idLink() {
+        phonenum = findViewById(R.id.phonenum);
+        nextbtn = findViewById(R.id.nextbtn);
+    }
+
+    public void listenerLink() {
+        nextbtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.nextbtn) {
+            phonetext = phonenum.getText().toString().trim();
+            if (TextUtils.isEmpty(phonetext)) {
+                phonenum.setError("Enter phone number");
+                phonenum.requestFocus();
+            } else {
+                if (!Validation.isValidPhone(phonetext)) {
+                    phonenum.setError("Enter Valid Phone no");
+                    phonenum.requestFocus();
+                } else {
+                    Intent intent = new Intent(MobileNumberInput.this, OtpInputActivity.class);
+                    intent.putExtra("phone",phonetext);
+                    if (android.os.Build.VERSION.SDK_INT >= JELLY_BEAN) {
+                        ActivityOptions options = ActivityOptions.makeCustomAnimation(MobileNumberInput.this, R.anim.fade_in, R.anim.fade_out);
+                        startActivity(intent, options.toBundle());
+                    } else {
+                        startActivity(intent);
+                    }
+
+                }
+            }
+
+        }
     }
 }
+
---
+++ app/src/main/java/peoplecitygroup/neuugen/Validation.java
@@ -0,0 +1,34 @@
+package peoplecitygroup.neuugen;
+
+import android.util.Patterns;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Validation {
+    public static boolean isValidName( String name) {
+        String NAME_PATTERN = "^[a-zA-Z\\s]*$";
+        Pattern pattern = Pattern.compile(NAME_PATTERN);
+        Matcher matcher = pattern.matcher(name);
+        return matcher.matches();
+
+    }
+    public static boolean isValidPassword(String password){
+        if(password.length()>=8)
+            return true;
+        else
+            return false;
+    }
+    public static boolean isValidEmail(String mail) {
+        return Patterns.EMAIL_ADDRESS.matcher(mail).matches();
+    }
+
+
+    public static boolean isValidPhone(String target) {
+        if (target == null || target.length() < 10 || target.length() > 10) {
+            return false;
+        } else {
+            return android.util.Patterns.PHONE.matcher(target).matches();
+        }
+    }
+}
