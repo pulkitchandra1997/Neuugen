bfdccd97d1f9b834070211ee7701a38dd8c5273f
pranjal012 <pranjalv206@gmail.com>
2019-03-13 17:09:37 +0530

    ghi

--- app/src/main/java/peoplecitygroup/neuugen/HomeFrag.java
+++ app/src/main/java/peoplecitygroup/neuugen/HomeFrag.java
@@ -5,15 +5,23 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.SearchView;
+
+import com.synnapps.carouselview.CarouselView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatEditText;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+
+import com.synnapps.carouselview.ImageListener;
 
 import static android.os.Build.VERSION_CODES.JELLY_BEAN;
 
@@ -23,6 +31,9 @@ public class HomeFrag extends Fragment implements View.OnClickListener {
 
     LinearLayout location;
     View v;
+    CarouselView carouselView;
+    int[] sampleImages = {R.drawable.defaultpic, R.drawable.camera, R.drawable.defaultpic, R.drawable.camera};
+
 
     @Nullable
     @Override
@@ -31,6 +42,22 @@ public class HomeFrag extends Fragment implements View.OnClickListener {
 
         idLink();
         listenerLink();
+
+
+
+
+        carouselView = v.findViewById(R.id.carouselView);
+        carouselView.setPageCount(sampleImages.length);
+
+        ImageListener imageListener = new ImageListener() {
+            @Override
+            public void setImageForPosition(int position, ImageView imageView) {
+                imageView.setImageResource(sampleImages[position]);
+            }
+        };
+        carouselView.setImageListener(imageListener);
+
+
 
         Typeface font = Typeface.createFromAsset(getActivity().getAssets(), "Font Awesome 5 Free-Solid-900.otf" );
         locationicon.setTypeface(font);
