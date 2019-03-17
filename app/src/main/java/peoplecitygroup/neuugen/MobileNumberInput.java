b1868f3e52ebe00294033ee0568d3e6b8407ae6a
pranjal012 <pranjalv206@gmail.com>
2019-03-17 10:56:15 +0530

    ghi

--- app/src/main/java/peoplecitygroup/neuugen/MobileNumberInput.java
+++ app/src/main/java/peoplecitygroup/neuugen/MobileNumberInput.java
@@ -51,6 +51,8 @@ public class MobileNumberInput extends AppCompatActivity implements View.OnClick
     String phonetext;
     private static final int PERMISSION_REQUEST_CODE = 200;
     List<String> numbers=new ArrayList<>();
+    int flag;
+    boolean doubleBackToExitPressedOnce = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,6 +64,8 @@ public class MobileNumberInput extends AppCompatActivity implements View.OnClick
         listenerLink();
         hideSoftKeyboard();
 
+
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -69,6 +73,30 @@ public class MobileNumberInput extends AppCompatActivity implements View.OnClick
             }
         }, 1000);
     }
+
+    public void onBackPressed() {
+        if(flag==0){
+            if (doubleBackToExitPressedOnce) {
+                Intent a = new Intent(Intent.ACTION_MAIN);
+                a.addCategory(Intent.CATEGORY_HOME);
+                a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(a);
+                return;
+            }
+            this.doubleBackToExitPressedOnce = true;
+            Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    doubleBackToExitPressedOnce=false;
+                }
+            }, 2000);
+        }
+        else{
+           this.finish();
+        }
+    }
+
 
     public void hideSoftKeyboard() {
         if (getCurrentFocus() != null) {
