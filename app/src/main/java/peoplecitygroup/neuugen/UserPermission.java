9b2fa660cb752444cbd5c039335726597809da98
PULKITCHANDRA <chandra.pulkit1997@gmail.com>
2019-03-17 21:53:54 +0530

    USer Details

--- app/src/main/java/peoplecitygroup/neuugen/UserPermission.java
+++ app/src/main/java/peoplecitygroup/neuugen/UserPermission.java
@@ -94,6 +94,7 @@ public class UserPermission extends AppCompatActivity implements View.OnClickLis
                         alertDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
                             @Override
                             public void onDismiss(DialogInterface dialog) {
+                                backtowelcome();
                                 //openNextActivity(intent);
                             }
                         });
@@ -119,11 +120,21 @@ public class UserPermission extends AppCompatActivity implements View.OnClickLis
                 break;
         }
     }
+
+    private void backtowelcome() {
+        finish();
+    }
+
     private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
         new AlertDialog.Builder(UserPermission.this)
                 .setMessage(message)
                 .setPositiveButton("OK", okListener)
-                .setNegativeButton("Cancel", null)
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        backtowelcome();
+                    }
+                })
                 .setIcon(R.mipmap.ic_launcher_round)
                 .setOnDismissListener(new DialogInterface.OnDismissListener() {
                     @Override
