6e4d0d4b5b3a9bcc9328ac3a3438f83c02a0ff7b
pranjal012 <pranjalv206@gmail.com>
2019-03-24 23:54:44 +0530

    ghi

--- app/src/main/java/peoplecitygroup/neuugen/UrlNeuugen.java
+++ app/src/main/java/peoplecitygroup/neuugen/UrlNeuugen.java
@@ -17,12 +17,36 @@ public class UrlNeuugen {
 
 
 
-
-
-
-
-
-
-
     public static final String salonServiceId="1";
+    public static final String mensalonServiceId="1";
+    public static final String womensalonServiceId="1";
+    public static final String menHaircutId="1";
+    public static final String menHaircutBeardId="1";
+    public static final String menPartymakeupId="1";
+    public static final String womenhaircutId="1";
+    public static final String womenPartymakeupId="1";
+    public static final String womenWeddingmakeupId="1";
+    public static final String appRepairInstallId="1";
+    public static final String repairingId="1";
+    public static final String installationId="1";
+    public static final String homeRenovationId="1";
+    public static final String plumbingId="1";
+    public static final String electricianId="1";
+    public static final String carpentryId="1";
+    public static final String learningServiceId="1";
+    public static final String dancingClassId="1";
+    public static final String actingClassId="1";
+    public static final String groomingClassId="1";
+    public static final String eventsServiceId="1";
+    public static final String photoVideoServiceId="1";
+    public static final String eventArrangementId="1";
+    public static final String preWedShootId="1";
+    public static final String weddingShootId="1";
+    public static final String eventPhotogaphyId="1";
+    public static final String normalWedShootId="1";
+    public static final String standardWedShootId="1";
+    public static final String premiumWedShootId="1";
+
+
+
 }
