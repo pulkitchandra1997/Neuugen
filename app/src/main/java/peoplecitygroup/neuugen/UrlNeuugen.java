3254b21aff4cb01f35043d94cf605a95e345d97a
pranjal012 <pranjalv206@gmail.com>
2019-03-24 23:56:01 +0530

    ghi

--- app/src/main/java/peoplecitygroup/neuugen/UrlNeuugen.java
+++ app/src/main/java/peoplecitygroup/neuugen/UrlNeuugen.java
@@ -18,34 +18,34 @@ public class UrlNeuugen {
 
 
     public static final String salonServiceId="1";
-    public static final String mensalonServiceId="1";
-    public static final String womensalonServiceId="1";
-    public static final String menHaircutId="1";
-    public static final String menHaircutBeardId="1";
-    public static final String menPartymakeupId="1";
-    public static final String womenhaircutId="1";
-    public static final String womenPartymakeupId="1";
-    public static final String womenWeddingmakeupId="1";
-    public static final String appRepairInstallId="1";
-    public static final String repairingId="1";
-    public static final String installationId="1";
-    public static final String homeRenovationId="1";
-    public static final String plumbingId="1";
-    public static final String electricianId="1";
-    public static final String carpentryId="1";
-    public static final String learningServiceId="1";
-    public static final String dancingClassId="1";
-    public static final String actingClassId="1";
-    public static final String groomingClassId="1";
-    public static final String eventsServiceId="1";
-    public static final String photoVideoServiceId="1";
-    public static final String eventArrangementId="1";
-    public static final String preWedShootId="1";
-    public static final String weddingShootId="1";
-    public static final String eventPhotogaphyId="1";
-    public static final String normalWedShootId="1";
-    public static final String standardWedShootId="1";
-    public static final String premiumWedShootId="1";
+    public static final String mensalonServiceId="2";
+    public static final String womensalonServiceId="3";
+    public static final String menHaircutId="4";
+    public static final String menHaircutBeardId="5";
+    public static final String menPartymakeupId="6";
+    public static final String womenhaircutId="7";
+    public static final String womenPartymakeupId="8";
+    public static final String womenWeddingmakeupId="9";
+    public static final String appRepairInstallId="10";
+    public static final String repairingId="11";
+    public static final String installationId="12";
+    public static final String homeRenovationId="13";
+    public static final String plumbingId="14";
+    public static final String electricianId="15";
+    public static final String carpentryId="16";
+    public static final String learningServiceId="17";
+    public static final String dancingClassId="18";
+    public static final String actingClassId="19";
+    public static final String groomingClassId="20";
+    public static final String eventsServiceId="21";
+    public static final String photoVideoServiceId="22";
+    public static final String eventArrangementId="23";
+    public static final String preWedShootId="24";
+    public static final String weddingShootId="25";
+    public static final String eventPhotogaphyId="26";
+    public static final String normalWedShootId="27";
+    public static final String standardWedShootId="28";
+    public static final String premiumWedShootId="29";
 
 
 
