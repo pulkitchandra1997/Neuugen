d8ea65b450101df4ea03a5b24d7d8e934708c50b
pranjal012 <pranjalv206@gmail.com>
2019-05-06 13:50:41 +0530

    ghi

--- app/src/main/java/peoplecitygroup/neuugen/AboutUs.java
+++ app/src/main/java/peoplecitygroup/neuugen/AboutUs.java
@@ -2,22 +2,40 @@ package peoplecitygroup.neuugen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import peoplecitygroup.neuugen.common_req_files.UrlNeuugen;
 
 public class AboutUs extends AppCompatActivity {
     WebView webview;
+    ProgressBar progress;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
 
+
+        progress=findViewById(R.id.progress);
         webview=findViewById(R.id.webview);
+
+        showProgress(true);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                showProgress(false);
+            }
+        },3000);
         WebSettings wb=webview.getSettings();
         webview.loadUrl(UrlNeuugen.aboutus);
         wb.setJavaScriptEnabled(true);
@@ -34,6 +52,31 @@ public class AboutUs extends AppCompatActivity {
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             view.loadUrl(url);
             return true;
+        }
+    }
+
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+
+            progress.setVisibility(show ? View.VISIBLE : View.GONE);
+            progress.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    progress.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            progress.setVisibility(show ? View.VISIBLE : View.GONE);
+
         }
     }
 }
--- app/src/main/java/peoplecitygroup/neuugen/Termsofuse.java
+++ app/src/main/java/peoplecitygroup/neuugen/Termsofuse.java
@@ -2,15 +2,47 @@ package peoplecitygroup.neuugen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import peoplecitygroup.neuugen.common_req_files.UrlNeuugen;
 
 public class Termsofuse extends AppCompatActivity {
     WebView webview;
+    ProgressBar progress;
+
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+
+            progress.setVisibility(show ? View.VISIBLE : View.GONE);
+            progress.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    progress.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            progress.setVisibility(show ? View.VISIBLE : View.GONE);
+
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +50,16 @@ public class Termsofuse extends AppCompatActivity {
         setContentView(R.layout.activity_termsofuse);
 
         webview=findViewById(R.id.webview);
+        progress=findViewById(R.id.progress);
+
+        showProgress(true);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                showProgress(false);
+            }
+        },3000);
+
         WebSettings wb=webview.getSettings();
         webview.loadUrl(UrlNeuugen.termsofuse);
         wb.setJavaScriptEnabled(true);
