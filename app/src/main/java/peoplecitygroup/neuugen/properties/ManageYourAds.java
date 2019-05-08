8a8d6672edeae7ef613b93efd937a7fc929a678a
Pulkit Chandra <chandra.pulkit1997@gmail.com>
2019-05-08 23:20:54 +0530

    showcase

--- app/src/main/java/peoplecitygroup/neuugen/properties/ManageYourAds.java
+++ app/src/main/java/peoplecitygroup/neuugen/properties/ManageYourAds.java
@@ -2,15 +2,21 @@ package peoplecitygroup.neuugen.properties;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
 import peoplecitygroup.neuugen.R;
 
 public class ManageYourAds extends AppCompatActivity {
+    ProgressDialog loading = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_your_ads);
+        loading = new ProgressDialog(ManageYourAds.this,R.style.AppCompatAlertDialogStyle);
+        loading.setCancelable(false);
+        loading.setMessage("Loading");
+        loading.setProgressStyle(ProgressDialog.STYLE_SPINNER);
     }
 }
